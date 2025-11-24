2be423903c5bea2481c0cf76d8b4b97232c38bfa
kashik09 <kashiku789@gmail.com>
2025-11-24 22:19:06 +0300

    Delete the instructions

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/* Instructions
-* Discount on an item
-* Profit after selling the item
-* Loss if the selling price is lower than the cost */
-
 // variables
 const item = "Car"
 const costPrice = 200000 // how much it ORIGINALLY cost
