939087e96d6324acc9c56deff398048a3bcce71b
kashik09 <kashiku789@gmail.com>
2025-11-24 16:33:03 +0300

    Define variables

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /* Instructions
 * Discount on an item
 * Profit after selling the item
-Loss if the selling price is lower than the cost */
+* Loss if the selling price is lower than the cost */
 
+// variables
+const item = "car"
+const costPrice = 200000
+const discount = 15
