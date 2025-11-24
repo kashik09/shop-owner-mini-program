0af0dc6be0f1b449b8eb223612aa6e1d40633ded
kashik09 <kashiku789@gmail.com>
2025-11-24 22:12:03 +0300

    Add comments

--- index.js
+++ index.js
@@ -4,14 +4,19 @@
 * Loss if the selling price is lower than the cost */
 
 // variables
-const item = "car"
-const costPrice = 200000
-const discount = 15
+const item = "Car"
+const costPrice = 200000 // how much it ORIGINALLY cost
+const discount = 15 // percentage discount
 
 // discount calculations
+// this calculates how much money the discount removes from the original cost
 const discountAmount = costPrice * (discount / 100)
-const discountedPrice = costPrice - discountAmount
+// the cost minus 15%, because i can't create a variable with percentage
 
+// the final price AFTER the discount
+const discountedPrice = costPrice - discountAmount // original - discount = the price it's sold at
+
+// console log results
 console.log("Item:", item)
 console.log("Discount Amount:", discountAmount)
 console.log("Price After Discount:", discountedPrice)
